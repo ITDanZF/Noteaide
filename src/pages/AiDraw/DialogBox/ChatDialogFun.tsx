a892bf9e280643f8274101656004630669c5fa1e
hejinyu <hejinyuhello@gmail.com>
2026-04-25 18:02:18 +0800

    style(AiDraw): 调整聊天对话框布局样式
    优化聊天对话框的布局结构，为每个Box添加明确的宽度和flex属性

--- src/pages/AiDraw/DialogBox/ChatDialogFun.tsx
+++ src/pages/AiDraw/DialogBox/ChatDialogFun.tsx
@@ -2,12 +2,17 @@ import { HStack, Box, Button } from '@chakra-ui/react';
 export default function ChatDialogFun() {
   return (
     <HStack>
-      <Box>上传图片</Box>
-      <Box>
+      {/* 左侧Box: 宽度自适应内容 */}
+      <Box w="auto">上传图片</Box>
+
+      {/* 中间Box: 占据所有剩余空间，内部内容靠左 */}
+      <Box flex="1" w="auto">
         <Button>选择模型</Button>
         <Button>深度思考</Button>
       </Box>
-      <Box>
+
+      {/* 右侧Box: 宽度自适应内容 */}
+      <Box w="auto">
         <Button>发送</Button>
       </Box>
     </HStack>
